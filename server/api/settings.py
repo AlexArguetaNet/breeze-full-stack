b96b6908bcdc6ca22747d1b43d3b7fdd62dfd1c9
Alejandro "Alex" Argueta <90729486+AlexArguetaNet@users.noreply.github.com>
2026-06-09 20:08:20 -0400

    Feature/forecast api setup (#1)
    * Created api endpoint to get the current forecast
    
    A new Django app called weather was created to handle
    fetching weather data from the Open Weather Map API.
    The weather module in the services folder contains the
    get_current function uses the request module to call this API.
    The get_current function is called in views.py to send
    a response containing data about the current weather forecast.
    The forecast location is determined by the query variable q when
    calling the Breeze API.
    
    * Modified endpoint in DRF
    
    In services/weather.py, the get_current function
    was changed to extract only the necessary weather
    information needed for the frontend. This was done
    to make the JSON response easier to understand and
    read.
    
    ---------
    
    Co-authored-by: AlexArguetaNet <aleexarguetanet@gmail.com>

--- server/api/settings.py
+++ server/api/settings.py
@@ -16,6 +16,9 @@ import os
 
 load_dotenv()
 
+# API key to use openweathermap api
+API_KEY = os.getenv("API_KEY")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
---
+++ server/weather/services/weather.py
@@ -0,0 +1,43 @@
+from django.conf import settings
+import requests
+from requests.exceptions import (Timeout, ConnectionError, HTTPError)
+
+API_KEY = settings.API_KEY
+
+
+def get_current(city: str) -> dict:
+    try:
+        
+        res = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={API_KEY}")
+        res.raise_for_status()
+
+        data = res.json()
+
+        # Extract necessary data
+        icon = data["weather"][0]["icon"]
+        main = data["weather"][0]["main"]
+        temp = data["main"]["temp"]
+        wind = data["wind"]
+        sunrise = data["sys"]["sunrise"]
+        sunset = data["sys"]["sunset"]
+        dt = data["dt"]
+        tz_offset = data["timezone"]
+        name = data["name"]
+
+        return {
+            "icon": icon,
+            "main": main,
+            "temp": temp,
+            "wind": wind,
+            "sunrise": sunrise,
+            "sunset": sunset,
+            "dt": dt,
+            "name": name
+        }
+
+    except Timeout:
+        return {"err": "Request timed out"}
+    except HTTPError as e:
+        return {"err": "HTTP error", "status": e.response.status_code}
+    except ConnectionError:
+        return {"err": "Weather service cannot be reached"}
